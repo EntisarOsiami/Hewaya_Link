7a70f645cebb6f5ec0a6f79d1ca3f20cb8584206
AdelAlosaimi050 <adilalosimi050@gmail.com>
2023-10-13 19:46:50 +0300

    remove secret info

--- backend/config/cloudinaryConfig.js
+++ backend/config/cloudinaryConfig.js
@@ -3,8 +3,8 @@ import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
   cloud_name: 'hewaya-link',
-  api_key: '394465281295285',
-  api_secret: 'PbvLobUwAMEeLgRvZfJRMN4U9cM',
+  api_key: '',
+  api_secret: '',
 });
 
 export default cloudinary;
