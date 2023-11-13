33fab45d603b6e865785064160579f0ad725d554
EntisarOsiami <code.g.light@gmail.com>
2023-11-13 15:56:15 +0300

    Working on the gallery feature

--- frontend/src/Screens/GalleryScreen.jsx
+++ frontend/src/Screens/GalleryScreen.jsx
@@ -1,9 +1,37 @@
+import  { useState, useEffect } from 'react';
 import UserGallery from '../Components/UserGallery.jsx';
 import UploadComponent from '../Components/UploadImage.jsx';
 import { useSelector } from 'react-redux';
+import axios from 'axios';
 
 const GalleryScreen = () => {
-  const { isAuthenticated } = useSelector((state) => state.auth); 
+  const { isAuthenticated } = useSelector((state) => state.auth);
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const fetchImages = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get('/api/gallery/images');
+      setImages(response.data.data.images);
+      setError('');
+    } catch (error) {
+      console.error('Error fetching images', error);
+      setError('Failed to fetch images.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  
+  const handleImageUpload = () => {
+    fetchImages();
+  };
 
   return (
     <div className="GalleryScreen-container">
@@ -11,10 +39,10 @@ const GalleryScreen = () => {
       {isAuthenticated && (
         <>
           <h2 className="GalleryScreen-subheader">Upload New Image</h2>
-          <UploadComponent />
+          <UploadComponent onImageUpload={handleImageUpload} />
         </>
       )}
-      <UserGallery />
+      <UserGallery images={images} loading={loading} error={error} />
     </div>
   );
 };
