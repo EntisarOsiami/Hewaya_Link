4db604ef96ba49aab84060a629530d214f46018d
EntisarOsiami <code.g.light@gmail.com>
2023-11-16 14:19:19 +0300

    refactor: add multerConfig.js.

--- backend/routes/galleryRoutes.js
+++ backend/routes/galleryRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { uploadToCloudinary, getAllImages, deleteFromCloudinary } from '../Controllers/galleryController.js';
 import { authenticateUser } from '../middleware/AuthMiddleware.js';
+import upload from '../config/multerConfig.js';
 
 const router = Router();
 
-router.post('/upload', authenticateUser, uploadToCloudinary);
+router.post('/upload', authenticateUser, upload.single('image'), uploadToCloudinary);
 router.get('/images', authenticateUser, getAllImages);
 router.delete('/images/:id', deleteFromCloudinary);
 
