f807b92a1e9c62507226541debdcdd2b54bc1225
EntisarOsiami <code.g.light@gmail.com>
2023-11-20 18:14:03 +0300

    admin screen somewhat up , :S

--- frontend/src/Components/AdminComponents/userManagement.jsx
+++ frontend/src/Components/AdminComponents/userManagement.jsx
@@ -1,4 +1,4 @@
-import  { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const UserManagement = () => {
@@ -10,8 +10,9 @@ const UserManagement = () => {
 
   const fetchUsers = async () => {
     try {
-      const response = await axios.get('/admin/users');
-      setUsers(response.data);
+      const response = await axios.get('/api/admin/users');
+      console.log(response.data.data);
+      setUsers(response.data.data);
     } catch (error) {
       console.error('Failed to fetch users:', error);
     }
@@ -19,16 +20,16 @@ const UserManagement = () => {
 
   const handleDisable = async (userId) => {
     try {
-      await axios.put(`/admin/disable/${userId}`);
+      await axios.put(`/api/admin/disable/${userId}`);
       fetchUsers(); // Refresh the users list
     } catch (error) {
       console.error('Failed to disable user:', error);
     }
   };
-
+  
   const handleEnable = async (userId) => {
     try {
-      await axios.put(`/admin/enable/${userId}`);
+      await axios.put(`/api/admin/enable/${userId}`);
       fetchUsers(); // Refresh the users list
     } catch (error) {
       console.error('Failed to enable user:', error);
@@ -40,14 +41,16 @@ const UserManagement = () => {
       <h1>User Management</h1>
       {users.map(user => (
         <div key={user._id}>
-          <h2>{user.name}</h2>
-          <p>{user.email}</p>
+          <h2>{user.name.firstName} {user.name.lastName}</h2>
+          <p>Email: {user.email.address} (Verified: {user.email.verified ? 'Yes' : 'No'})</p>
+          <p>Disabled: {user.disabled ? 'Yes' : 'No'}</p> 
           <button onClick={() => handleDisable(user._id)}>Disable</button>
           <button onClick={() => handleEnable(user._id)}>Enable</button>
         </div>
       ))}
     </div>
   );
+  
 };
 
 export default UserManagement;
