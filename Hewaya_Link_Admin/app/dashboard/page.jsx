a32bee1383920f97c2e63ede0e89bc8c3d58f075
AdelAlosaimi050 <adilalosimi050@gmail.com>
2023-11-24 15:00:52 +0300

    charts

--- Hewaya_Link_Admin/app/dashboard/page.jsx
+++ Hewaya_Link_Admin/app/dashboard/page.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from '../ui/dashboard/dashboard.module.css';
 import Card from '../ui/dashboard/card/card';
-import Chart from '../ui/dashboard/chart/chart';
 import Rightbar from '../ui/dashboard/rightbar/rightbar';
 import Transactions from '../ui/dashboard/transactions/transactions';
+import Chart from '../ui/dashboard/chart/chart';
 import { cards } from '../lib/data';
 
 const Dashboard = () => {
