48665737aaec50589a6a4a82aaa5e4f8dd497c31
AdelAlosaimi050 <adilalosimi050@gmail.com>
2023-11-24 15:08:31 +0300

    right bar

--- Hewaya_Link_Admin/app/ui/dashboard/rightbar/rightbar.jsx
+++ Hewaya_Link_Admin/app/ui/dashboard/rightbar/rightbar.jsx
@@ -1,9 +1,30 @@
-import { MdSupervisedUserCircle } from "react-icons/md";
 import styles from "./rightbar.module.css";
-
+import { MdPlayCircleFilled, MdReadMore } from "react-icons/md";
+import Image from "next/image";
 const Rightbar = ({ item }) => {
   return (
-   <div></div>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.bgContainer}>
+          <Image className={styles.bg} src="/astronaut.png" alt="" fill />
+        </div>
+        <div className={styles.text}>
+          <span className={styles.notification}>🔥 Available Now</span>
+          <h3 className={styles.title}>
+            How to use the new version of the admin dashboard?
+          </h3>
+          <span className={styles.subtitle}>Takes 4 minutes to learn</span>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Reprehenderit eius libero perspiciatis recusandae possimus.
+          </p>
+          <button className={styles.button}>
+            <MdPlayCircleFilled />
+            Watch
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
