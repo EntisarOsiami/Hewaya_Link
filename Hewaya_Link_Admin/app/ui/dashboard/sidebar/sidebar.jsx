7231ef0d25b4496849e904cff9c9d4a8dc815e66
AdelAlosaimi050 <adilalosimi050@gmail.com>
2023-11-24 13:37:01 +0300

    add icons

--- Hewaya_Link_Admin/app/ui/dashboard/sidebar/sidebar.jsx
+++ Hewaya_Link_Admin/app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,8 +1,74 @@
 import React from 'react';
+import styles from '../sidebar/sidebar.module.css';
+import { MdWork, MdAnalytics, MdPeople, MdDashboard, 
+  MdAttachMoney,MdSupervisedUserCircle, 
+  MdShoppingBag,MdHelpCenter ,MdOutlineSettings} from "react-icons/md";
+const menuItems = [
+  {
+    title: "Pages",
+    list: [
+      {
+        title: "Dashboard",
+        path: "/dashboard",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Users",
+        path: "/dashboard/users",
+        icon: <MdSupervisedUserCircle />,
+      },
+      {
+        title: "Products",
+        path: "/dashboard/products",
+        icon: <MdShoppingBag />,
+      },
+      {
+        title: "Transactions",
+        path: "/dashboard/transactions",
+        icon: <MdAttachMoney />,
+      },
+    ],
+  },
+  {
+    title: "Analytics",
+    list: [
+      {
+        title: "Revenue",
+        path: "/dashboard/revenue",
+        icon: <MdWork />,
+      },
+      {
+        title: "Reports",
+        path: "/dashboard/reports",
+        icon: <MdAnalytics />,
+      },
+      {
+        title: "Teams",
+        path: "/dashboard/teams",
+        icon: <MdPeople />,
+      },
+    ],
+  },
+  {
+    title: "User",
+    list: [
+      {
+        title: "Settings",
+        path: "/dashboard/settings",
+        icon: <MdOutlineSettings />,
+      },
+      {
+        title: "Help",
+        path: "/dashboard/help",
+        icon: <MdHelpCenter />,
+      },
+    ],
+  },
+];
 
 const Sidebar = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Sidebar</h1>
     </div>
   );
