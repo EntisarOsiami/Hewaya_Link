d890d8f3eb41188b4abf10d1f517157d33e1c4cd
AdelAlosaimi050 <adilalosimi050@gmail.com>
2023-11-24 20:44:36 +0300

    login page

--- Hewaya_Link_Admin/app/login/page.jsx
+++ Hewaya_Link_Admin/app/login/page.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import styles from '../ui/login/login.module.css';
 const Login = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
       <h1>Login</h1>
+
+        <input type="username" name="name" placeholder="username"/>
+        <input type="password" name="password" placeholder="username" />
+        <button>Login</button>
+      </form>
     </div>
   );
 };
